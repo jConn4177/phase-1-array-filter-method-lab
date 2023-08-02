b51062400fd77b9836e1b0eb3ab273c84c61ad1e
Jason <jconn99801@gmail.com>
2023-08-02 12:13:25 -0800

    lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const nameMatchMid = drivers.filter((driverName) => {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+  return nameMatchMid;
+}
+
+function fuzzyMatch(drivers, name) {
+  const nameMatchSoft = drivers.filter((driverName) => {
+    return driverName[0] === name[0];
+  });
+  return nameMatchSoft;
+}
+
+function matchName(drivers, name) {
+  const nameMatchHard = drivers.filter((driver) => driver.name === name);
+  return nameMatchHard;
+}
